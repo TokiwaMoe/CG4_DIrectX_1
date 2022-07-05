9ffe1b8a03e375de38fd2ccd1aae8c3f06c575cb
125moti <k020g1385@g.neec.ac.jp>
2022-07-05 10:42:23 +0900

    ポストエフェクトのピクセルシェーダー変えた

--- Resource/shaders/PostEffectPS.hlsl
+++ Resource/shaders/PostEffectPS.hlsl
@@ -6,27 +6,29 @@ SamplerState smp : register(s0);		//0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	float4 colortex0 = tex0.Sample(smp, input.uv);
+	float4 colortex0/* = tex0.Sample(smp, input.uv)*/;
 	float4 colortex1 = tex1.Sample(smp, input.uv);
 
-	float4 color = colortex0;
+	//ぼかし
+	const float window_width = 1280;
+	const float window_height = 720;
+	float u = 1.0f / window_width;
+	float v = 1.0f / window_height;
+	//float4 color = { 0, 0, 0, 0 };
+
+	for (int y = 0; y < 11; y++) {
+		for (int x = 0; x < 11; x++)
+			colortex0 += tex0.Sample(smp, input.uv + float2(u * x, v * y));
+	}
+
+	float4 color = (colortex0.rbg / float(11 * 11), 1);
 	if (fmod(input.uv.y, 0.1f) < 0.05f)
 	{
 		color = colortex1;
 	}
 
-	return float4(color.rgb, 1);
-	//ぼかし
-	/*const float window_width = 1280;
-	const float window_height = 720;
-	float u = 1.0f / window_width;
-	float v = 1.0f / window_height;
-	float4 color = { 0, 0, 0, 0 };
-
-	for (int y = 0; y < 11; y++) {
-		for (int x = 0; x < 11; x++)
-			color += tex.Sample(smp, input.uv + float2(u * x, v * y));
-	}*/
+	return float4(color.rbg, 1);
+	
 
 	//return float4(color.rbg / float(11 * 11), 1);
 
