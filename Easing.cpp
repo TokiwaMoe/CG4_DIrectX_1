cad20f304a96a771a844a1515e31a4f30f2ee7ad
125moti <k020g1385@g.neec.ac.jp>
2022-08-21 21:09:48 +0900

    バウンドできた

--- Easing.cpp
+++ Easing.cpp
@@ -37,7 +37,7 @@ XMFLOAT3 Easing::easeOut(XMFLOAT3 start, XMFLOAT3 end, float flame)
 	difference = { end.x - start.x, end.y - start.y, end.z - start.z };
 	time = flame / maxflame;
 	float v = Bounce_out(time);
-	position.x = difference.x * v + start.x;
+	position.x = difference.x * time + start.x;
 	position.y = difference.y * v + start.y;
 	position.z = difference.z * v + start.z;
 	return position;
